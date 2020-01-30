445ac7dc7106a4ccbc43b6a9b21886bd14426034
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-01-30 12:25:29 +0100

    Add CI, upgrade package versions, minor scaffolding

--- src/test/java/inf112/skeleton/app/AppTest.java
+++ src/test/java/inf112/skeleton/app/AppTest.java
@@ -1,7 +1,8 @@
 package inf112.skeleton.app;
 
-import static org.junit.Assert.assertTrue;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
