c4925be12a58d203be6ef45e794817f7f1cdab3a
ruy010 <Silje.moen@student.uib.no>
2020-03-07 18:30:22 +0100

    Made an interface for the future card class.

---
+++ src/main/java/biblioteket/roborally/programcards/ICard.java
@@ -0,0 +1,27 @@
+package biblioteket.roborally.programcards;
+
+import biblioteket.roborally.actors.IRobot;
+
+public interface ICard {
+
+    /**
+     * Get which type the card is
+     *
+     * @return which type the card is
+     */
+    CardType getType();
+
+    /**
+     * Get the priority number of the card
+     *
+     * @return priority number of card
+     */
+    int getPriorityNumber();
+
+    /**
+     * Make the robot do the action of the program card
+     *
+     * @param robot
+     */
+    void doCardAction(IRobot robot);
+}
