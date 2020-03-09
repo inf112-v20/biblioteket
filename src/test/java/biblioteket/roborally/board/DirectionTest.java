cc9ed014484a1c4255aa14245a45d2665bb3b315
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-03-09 12:28:28 +0100

    Add Tile enum for fetching what tile is where

--- src/test/java/biblioteket/roborally/board/DirectionTest.java
+++ src/test/java/biblioteket/roborally/board/DirectionTest.java
@@ -1,6 +1,5 @@
-package biblioteket.roborally;
+package biblioteket.roborally.board;
 
-import biblioteket.roborally.board.Direction;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
