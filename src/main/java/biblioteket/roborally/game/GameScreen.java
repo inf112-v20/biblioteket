e22557c5f3eaa34d6eff5da91fbfe69ac31670a2
Sondre Nilsen <nilsen.sondre@gmail.com>
2020-03-26 14:54:36 +0100

    Fix player spawning on correct spawn point
    Closes #87

--- src/main/java/biblioteket/roborally/game/GameScreen.java
+++ src/main/java/biblioteket/roborally/game/GameScreen.java
@@ -45,7 +45,7 @@ public class GameScreen implements Screen {
 
         this.players = new ArrayList<>();
 
-        for (int i = 1; i <= 1; i++) {
+        for (int i = 0; i < 1; i++) {
             Player player = new Player(new TiledMapTileLayer.Cell().setTile(new StaticTiledMapTile(playerTextureSplit[0][0])));
             currentPlayer = player;
             players.add(player);
