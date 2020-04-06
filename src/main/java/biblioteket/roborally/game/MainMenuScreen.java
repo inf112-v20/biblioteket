453088a74e6c8859f2ab6b0c64574b56fc6dfdb2
tor-kristian <Tor.K.Ellingsen@student.uib.no>
2020-04-06 17:59:19 +0200

    Fixed resizing in MainMenuScreen and MapSelect

--- src/main/java/biblioteket/roborally/game/MainMenuScreen.java
+++ src/main/java/biblioteket/roborally/game/MainMenuScreen.java
@@ -13,15 +13,18 @@ import com.badlogic.gdx.graphics.Texture;
  */
 public class MainMenuScreen implements Screen {
     private final RoboRally game;
-    private final OrthographicCamera camera;
-    private final Texture logo;
-    private final Texture background;
-    private final Texture playPre;
-    private final Texture playPost;
-    private final Texture quitPre;
-    private final Texture quitPost;
-    private static final int BUTTON_HEIGHT = 250;
-    private static final int BUTTON_WIDTH = 180;
+    OrthographicCamera camera;
+    private Texture logo;
+    private Texture background;
+    private Texture playPre;
+    private Texture playPost;
+    private Texture quitPre;
+    private Texture quitPost;
+    private float buttonHeight;
+    private float buttonWidth;
+    private float center;
+    private float startY;
+    private float exitY;
 
 
     public MainMenuScreen(final RoboRally game) {
@@ -37,6 +40,8 @@ public class MainMenuScreen implements Screen {
         quitPre = new Texture("assets/buttons/quitPre.png");
         quitPost = new Texture("assets/buttons/quitPost.png");
 
+
+
     }
 
     @Override
@@ -44,36 +49,45 @@ public class MainMenuScreen implements Screen {
 
     }
 
+    public void buttonSize() {
+        center = camera.viewportWidth / 2 - buttonWidth / 2;
+        buttonHeight =  camera.viewportHeight/(256/100);
+        buttonWidth = camera.viewportHeight/(356/100);
+        startY = camera.viewportHeight/(6);
+        exitY = camera.viewportHeight/130;
+    }
+
     @Override
     public void render(float delta) {
-        int center = (int) camera.viewportWidth / 2 - BUTTON_WIDTH / 2;
+        buttonSize();
 
         Gdx.gl.glClearColor(0, 0, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         camera.update();
-
         game.getBatch().setProjectionMatrix(camera.combined);
-        game.getBatch().begin();
-        game.getBatch().draw(background, 0, 0, 700, 700);
-        game.getBatch().draw(logo, 70, 340);
-        game.getBatch().draw(playPre, center, camera.viewportHeight / 4, BUTTON_WIDTH, BUTTON_HEIGHT);
-        game.getBatch().draw(quitPre, center, camera.viewportHeight / 10, BUTTON_WIDTH, BUTTON_HEIGHT);
 
-        if (Gdx.input.getX() < center + BUTTON_WIDTH && Gdx.input.getX() > center && camera.viewportHeight - Gdx.input.getY() < camera.viewportHeight / 4 + BUTTON_WIDTH && camera.viewportHeight - Gdx.input.getY() > camera.viewportHeight / 4 + BUTTON_WIDTH / (1.5)) {
-            game.getBatch().draw(quitPost, center, camera.viewportHeight / 10, BUTTON_WIDTH, BUTTON_HEIGHT);
+        game.getBatch().begin();
+        game.getBatch().draw(background, 0, 0, camera.viewportWidth, camera.viewportHeight);
+        game.getBatch().draw(logo, camera.viewportWidth/2 - logo.getWidth()/2*camera.viewportHeight/640, camera.viewportHeight/2,logo.getWidth()*camera.viewportHeight/640,logo.getHeight()*camera.viewportHeight/640);
+        game.getBatch().draw(playPre, center, startY, buttonWidth, buttonHeight);
+        game.getBatch().draw(quitPre, center, exitY, buttonWidth, buttonHeight);
+
+        if (Gdx.input.getX() < center + buttonWidth && Gdx.input.getX() > center && camera.viewportHeight - Gdx.input.getY() < startY + buttonHeight/1.35 && camera.viewportHeight - Gdx.input.getY() > startY + buttonWidth / (1.35)) {
+            game.getBatch().draw(quitPost, center, exitY, buttonWidth, buttonHeight);
             if (Gdx.input.isTouched()) {
-                game.setScreen(new GameScreen(game));
+                game.setScreen(new MapSelect(game));
                 dispose();
             }
-        } else if (Gdx.input.getX() < center + BUTTON_WIDTH && Gdx.input.getX() > center && camera.viewportHeight - Gdx.input.getY() < camera.viewportHeight / 7 + BUTTON_WIDTH && camera.viewportHeight - Gdx.input.getY() > camera.viewportHeight / 10 + BUTTON_WIDTH / (1.5)) {
-            game.getBatch().draw(playPost, center, camera.viewportHeight / 4, BUTTON_WIDTH, BUTTON_HEIGHT);
+        } else if (Gdx.input.getX() < center + buttonWidth && Gdx.input.getX() > center && camera.viewportHeight - Gdx.input.getY() < exitY + buttonHeight/1.35 && camera.viewportHeight - Gdx.input.getY() > exitY + buttonWidth / (1.35)) {
+            game.getBatch().draw(playPost, center, startY, buttonWidth, buttonHeight);
             if (Gdx.input.isTouched()) {
                 Gdx.app.exit();
+
             }
         } else {
-            game.getBatch().draw(playPost, center, camera.viewportHeight / 4, BUTTON_WIDTH, BUTTON_HEIGHT);
-            game.getBatch().draw(quitPost, center, camera.viewportHeight / 10, BUTTON_WIDTH, BUTTON_HEIGHT);
+            game.getBatch().draw(playPost, center, startY, buttonWidth, buttonHeight);
+            game.getBatch().draw(quitPost, center, exitY, buttonWidth, buttonHeight);
 
         }
 
@@ -82,7 +96,9 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        camera.setToOrtho(false, width, height);
+        camera.update();
+        game.getBatch().setProjectionMatrix(camera.combined);
     }
 
     @Override
@@ -104,4 +120,5 @@ public class MainMenuScreen implements Screen {
     public void dispose() {
 
     }
+
 }
---
+++ src/main/java/biblioteket/roborally/game/MapSelect.java
@@ -0,0 +1,111 @@
+package biblioteket.roborally.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+
+public class MapSelect implements Screen {
+
+
+    private final RoboRally game;
+    OrthographicCamera camera;
+    private Texture background;
+    private Texture dizzyPre;
+    private Texture dizzyPost;
+    private Texture riskyPre;
+    private Texture riskyPost;
+    private BitmapFont font;
+    private Texture selectMap;
+
+    public MapSelect(final RoboRally game) {
+        this.game = game;
+        this.camera = new OrthographicCamera();
+        camera.setToOrtho(false, 640, 640);
+
+        background = new Texture("assets/background2.jpg");
+        dizzyPre = new Texture("assets/gamemaps/dizzyPre.png");
+        dizzyPost = new Texture("assets/gamemaps/dizzyPost.png");
+        riskyPre = new Texture("assets/gamemaps/riskyPre.png");
+        riskyPost = new Texture("assets/gamemaps/riskyPost.png");
+        font = new BitmapFont();
+        selectMap = new Texture("assets/selectMap.png");
+    }
+
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float v) {
+
+        float mapWidth = camera.viewportHeight/(3.2f);
+        float mapHeight = camera.viewportHeight/(2.42f);
+        float center = camera.viewportWidth/2;
+        float dizzyX = center - camera.viewportWidth / 9 - mapWidth;
+        float riskyX = center + camera.viewportWidth / 9;
+        float mapY = camera.viewportHeight / 3;
+
+        game.getBatch().setProjectionMatrix(camera.combined);
+        game.getBatch().begin();
+        game.getBatch().draw(background, 0, 0, camera.viewportWidth, camera.viewportHeight);
+        //game.getBatch().draw(logo, 70, 340);
+        game.getBatch().draw(dizzyPre, dizzyX, mapY, mapWidth, mapHeight);
+        game.getBatch().draw(riskyPre, riskyX , mapY, mapWidth, mapHeight);
+        game.getBatch().draw(selectMap, camera.viewportWidth/2 - selectMap.getWidth()/2*camera.viewportHeight/640, camera.viewportHeight/2, selectMap.getWidth()*camera.viewportHeight/640,selectMap.getHeight()*camera.viewportHeight/640);
+        // game.getBatch().draw(logo, camera.viewportWidth/2 - logo.getWidth()/2*camera.viewportHeight/640, camera.viewportHeight/2,logo.getWidth()*camera.viewportHeight/640,logo.getHeight()*camera.viewportHeight/640);
+        font.draw(game.getBatch(), "Dizzy Dash", camera.viewportWidth / 7 + 70 , camera.viewportHeight / (390/100) );
+        font.draw(game.getBatch(), "Risky Exchange", camera.viewportWidth / 2 + 100, camera.viewportHeight / (390/100) );
+
+
+        if (Gdx.input.getX() < dizzyX + mapWidth && Gdx.input.getX() > dizzyX && camera.viewportHeight - Gdx.input.getY() < mapY + mapHeight && camera.viewportHeight - Gdx.input.getY() > mapY ) {
+            game.getBatch().draw(dizzyPost, dizzyX, mapY, mapWidth, mapHeight);
+            if (Gdx.input.isTouched()) {
+                GameScreen.map = "assets/DizzyDash.tmx";
+                game.setScreen(new GameScreen(game));
+                dispose();
+
+            }
+        }
+        else if (Gdx.input.getX() < riskyX + mapWidth && Gdx.input.getX() > riskyX && camera.viewportHeight - Gdx.input.getY() < mapY + mapHeight && camera.viewportHeight - Gdx.input.getY() > mapY ) {
+            game.getBatch().draw(riskyPost, riskyX, mapY, mapWidth, mapHeight);
+            if (Gdx.input.isTouched()) {
+                GameScreen.map = "assets/RiskyExchange.tmx";
+                game.setScreen(new GameScreen(game));
+                dispose();
+            }
+        }
+        game.getBatch().end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        this.camera = new OrthographicCamera();
+        camera.setToOrtho(false, width, height);
+        camera.update();
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
+
